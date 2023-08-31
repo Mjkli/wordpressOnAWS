4f5399c9c00778dd2ffdca5457364206b82768cf
Anthony <anthonybruno63@yahoo.com>
2023-08-31 16:07:42 -0700

    Feature/cloudfront (#4)
    * adding change rules to test action so that builds are smaller
    
    * adding if to infra so it always builds but will wait for image to update if running
    
    * cloudfront is now connected to alb
    
    * adding curl check of cloudfront
    
    * getting variable of dns output
    
    * command check
    
    * adding check to cloudfront
    
    * add cloudfront to github output
    
    * adding https before curl call
    
    ---------
    
    Co-authored-by: Mjkli <Mjkli@users.noreply.github.com>

---
+++ infrastructure/tf/cloudfront.tf
@@ -0,0 +1,41 @@
+resource "aws_cloudfront_distribution" "cf_dist" {
+
+    enabled = true
+
+    origin {
+
+      domain_name = aws_lb.app-lb.dns_name
+      origin_id = "wp-origin"
+      custom_origin_config {
+        http_port = 80
+        https_port = 443
+        origin_protocol_policy = "http-only"
+        origin_ssl_protocols = ["SSLv3","TLSv1.2"]
+      }
+    }
+
+
+    default_cache_behavior {
+        allowed_methods = ["GET","HEAD"]
+        cached_methods = ["GET","HEAD"]
+        target_origin_id = "wp-origin"
+        cache_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+        origin_request_policy_id = "216adef6-5c7f-47e4-b989-5492eafa07d3"
+        viewer_protocol_policy = "redirect-to-https"
+        min_ttl = 0
+        default_ttl = 0
+        max_ttl = 0
+    }
+
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+            locations = []
+        }
+    }
+
+}
