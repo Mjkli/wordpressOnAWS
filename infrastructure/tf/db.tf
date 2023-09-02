d49c63d782872bf2b2794de4ad2f7d202755cd47
Mjkli <Mjkli@users.noreply.github.com>
2023-09-01 19:21:54 -0700

    adding statelock to prevent multiple runs on state file

--- infrastructure/tf/db.tf
+++ infrastructure/tf/db.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "wp-db" {
     db_name = "wp-db"
     engine = "mysql"
     engine_version = "8.0.34"
-    instance_class = "db.t3.micro"
+    instance_class = "db.t4g.micro"
     username = "db_admin"
     password = "replace_me"
     db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
@@ -25,7 +25,7 @@ resource "aws_elasticache_subnet_group" "memcache_sub_group" {
 resource "aws_elasticache_cluster" "wp-cache" {
     cluster_id = "wp-cache"
     engine = "memcached"
-    node_type = "t4g.micro"
+    node_type = "cache.t4g.micro"
     num_cache_nodes = 1
     port = 11211
     subnet_group_name = aws_elasticache_subnet_group.memcache_sub_group.name
