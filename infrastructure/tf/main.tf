64353c3180b1ee048fa048df24ec7962174d2217
Anthony <anthonybruno63@yahoo.com>
2023-09-02 08:36:59 -0700

    Feature/database (#5)
    * initial database config
    
    * adding memcache and rds instance
    
    * adding memcache and rds instance
    
    * adding statelock to prevent multiple runs on state file
    
    * manually created dynamo db table for state lock
    
    * fix db names
    
    * not adding efs
    
    ---------
    
    Co-authored-by: Mjkli <Mjkli@users.noreply.github.com>

--- infrastructure/tf/main.tf
+++ infrastructure/tf/main.tf
@@ -6,6 +6,7 @@ terraform {
   backend "s3"{
     encrypt = true
     bucket = "state-storage-mjkli"
+    dynamodb_table = "wp-state-lock"
     key = "wordpressOnAWS-tf-state"
     region = "us-west-1"
   }
