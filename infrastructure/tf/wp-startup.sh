d8ffdef327cc077c95ed8207801414ba11a634eb
Mjkli <Mjkli@users.noreply.github.com>
2023-09-06 07:25:16 -0700

    verifying that the wordpress site is appying

--- infrastructure/tf/wp-startup.sh
+++ infrastructure/tf/wp-startup.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+# wait for machine to come up
+sleep 30
+
 # Mounting EFS target
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport efs-dns:/ /var/www/html/wordpress
-
 
 # Check if there is files in the efs directory if empty add default wordpress file else continue
 if [ -n "$(ls -A /var/www/html/wordpress 2>/dev/null)" ]
 then
-  curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /var/www/html/wordpress
-  sudo a2ensite wordpress
-  sudo a2enmod rewrite
-  sudo a2dissite 000-default
-  sudo service apache2 reload
+  echo "config files are here"
 else
-  echo "empty"
+  curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /var/www/html/wordpress
 fi
+
+sudo a2ensite wordpress
+sudo a2enmod rewrite
+sudo a2dissite 000-default
+sudo service apache2 reload
