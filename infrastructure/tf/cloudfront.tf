2900b5b37520fecde0587e184e0360209aab2621
Mjkli <Mjkli@users.noreply.github.com>
2023-09-12 15:57:17 -0700

    adjust cache_behavior

--- infrastructure/tf/cloudfront.tf
+++ infrastructure/tf/cloudfront.tf
@@ -3,14 +3,13 @@ resource "aws_cloudfront_distribution" "cf_dist" {
     enabled = true
 
     origin {
-
       domain_name = aws_lb.app-lb.dns_name
       origin_id = "wp-origin"
       custom_origin_config {
         http_port = 80
         https_port = 443
-        origin_protocol_policy = "https-only"
-        origin_ssl_protocols = ["SSLv3","TLSv1.2"]
+        origin_protocol_policy = "match-viewer"
+        origin_ssl_protocols = ["TLSv1.2","TLSv1.1","TLSv1"]
       }
     }
 
@@ -21,7 +20,7 @@ resource "aws_cloudfront_distribution" "cf_dist" {
         target_origin_id = "wp-origin"
         cache_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
         origin_request_policy_id = "216adef6-5c7f-47e4-b989-5492eafa07d3"
-        viewer_protocol_policy = "redirect-to-https"
+        viewer_protocol_policy = "https-only"
         min_ttl = 0
         default_ttl = 0
         max_ttl = 0
