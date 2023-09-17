e68029a413f0cb5981ebd39a23306ed7d9794929
Mjkli <Mjkli@users.noreply.github.com>
2023-09-16 18:59:17 -0700

    updating cloudfront settings

--- infrastructure/tf/cloudfront.tf
+++ infrastructure/tf/cloudfront.tf
@@ -1,6 +1,7 @@
 resource "aws_cloudfront_distribution" "cf_dist" {
 
     enabled = true
+    aliases = [ "wp.mjkli.com" ]
 
     origin {
       domain_name = aws_lb.app-lb.dns_name
@@ -8,15 +9,15 @@ resource "aws_cloudfront_distribution" "cf_dist" {
       custom_origin_config {
         http_port = 80
         https_port = 443
-        origin_protocol_policy = "https-only"
+        origin_protocol_policy = "match-viewer"
         origin_ssl_protocols = ["TLSv1.2","TLSv1.1","TLSv1"]
       }
     }
 
 
     default_cache_behavior {
-        allowed_methods = ["GET","HEAD"]
-        cached_methods = ["GET","HEAD"]
+        allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        cached_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
         target_origin_id = "wp-origin"
         cache_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
         origin_request_policy_id = "216adef6-5c7f-47e4-b989-5492eafa07d3"
@@ -24,6 +25,9 @@ resource "aws_cloudfront_distribution" "cf_dist" {
         min_ttl = 0
         default_ttl = 0
         max_ttl = 0
+        cookies {
+            forward = "all"
+        }
     }
 
     viewer_certificate {
