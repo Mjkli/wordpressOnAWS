58934b3ea0cad51d54896e2954cbaad22de38464
Mjkli <Mjkli@users.noreply.github.com>
2023-09-16 19:54:31 -0700

    adding validation in virginia

--- infrastructure/tf/acm.tf
+++ infrastructure/tf/acm.tf
@@ -10,6 +10,8 @@ resource "aws_acm_certificate" "wp-cert" {
 }
 
 resource "aws_acm_certificate_validation" "wp-cert-val" {
+    provider = aws.virginia
+    
     certificate_arn = aws_acm_certificate.wp-cert.arn
     validation_record_fqdns = [ for record in aws_route53_record.wp_cert_record : record.fqdn]
 }
