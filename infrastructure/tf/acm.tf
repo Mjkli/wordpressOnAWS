faa2a799a9f15d66a229b31ec356f01695508a37
Mjkli <Mjkli@users.noreply.github.com>
2023-09-18 08:12:59 -0700

    specifying wp-lb cert val region

--- infrastructure/tf/acm.tf
+++ infrastructure/tf/acm.tf
@@ -28,6 +28,7 @@ resource "aws_acm_certificate" "wp-lb-cert" {
 }
 
 resource "aws_acm_certificate_validation" "wp-lb-cert-val" {
+    provider = aws.cali
     certificate_arn = aws_acm_certificate.wp-cert.arn
     validation_record_fqdns = [ for record in aws_route53_record.wp_cert_record : record.fqdn]
 }
