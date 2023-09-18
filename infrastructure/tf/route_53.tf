57830857c609ee07e791527769e059ad9677a61a
Mjkli <Mjkli@users.noreply.github.com>
2023-09-17 17:30:10 -0700

    changing route53 record

--- infrastructure/tf/route_53.tf
+++ infrastructure/tf/route_53.tf
@@ -22,7 +22,7 @@ resource "aws_route53_record" "wp_cert_record" {
 resource "aws_route53_record" "wp" {
     zone_id = data.aws_route53_zone.mjkli_zone.zone_id
     name = "wp.mjkli.com"
-    type = "A"
+    type = "CNAME"
     ttl = "300"
     records = [aws_cloudfront_distribution.cf_dist.domain_name]
 
